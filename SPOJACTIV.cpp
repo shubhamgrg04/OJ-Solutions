11478a6314c896b33b40b83d84287757246f1701
Shubham Garg <shubhamgrg04@gmail.com>
2017-06-26 09:27:41 +0530

    SPOJ-ACTIV

--- SPOJACTIV.cpp
+++ SPOJACTIV.cpp
@@ -1,10 +1,53 @@
-#include <iostream>
- 
+#include  <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
- 
-inline int in(){int x; cin >> x; return x;}
- 
+
+int const MOD = 1e8;
+
+inline int in() { int x; cin >> x; return x;}
+
+typedef pair<int,int> pi;
+
+bool smallerfirst(pi a, pi b) {
+	if(a.first < b.first) return true;
+	return false;
+}
+
+bool smallersecond(pi a, pi b) {
+	if(a.second < b.second) return true;
+	return false;
+}
+
+int search(int a, vector<pi> &sort_first){
+	int low = 0, high = sort_first.size(), mid;
+	while(low<high){
+		mid = (low+high)/2;
+		if(sort_first[mid].first==a) break;
+		if(sort_first[mid].first>a && ((mid>0)?sort_first[mid-1].first<a:1) ) break;
+		if(sort_first[mid].first<a) low = mid+1;
+		if(sort_first[mid].first>a) high = mid-1;
+	}
+}
+
 int main(){
-	ios::sync_with_stdio(false);
+	int n;
+	while((n = in()) != -1) {
+		vector<pi> sort_first(n), sort_second(n);
+		vector<int> mem(n+1); mem[n] = 0;
+		int ans = 0;
+		for(int i=0; i<n; i++){
+			int start = in(), end = in();
+			sort_first[i] = make_pair(start,end);
+			sort_second[i] = make_pair(start,end);
+		}
+		sort(sort_first.begin(), sort_first.end(), smallerfirst);
+
+		for(int i=n-1; i>=0; i--){
+			mem[i] = (((i<n-1)?mem[i+1]:0) + 1 + mem[search(sort_first[i].second, sort_first)]) % MOD;
+		}
+		cout << ans << endl;
+	}
 	return 0;
 }
